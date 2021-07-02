9ff714191909b521521ed002b03b721def9b8ed8
tevoran <j.pfeiffer93@gmx.de>
2021-07-02 18:51:22 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,15 @@ int main()
 	t3v::engine& te=t3v::engine::get();
 	
 	//setting properties
-	te.set_resx(1920);
-	te.set_resy(1080);
-	te.set_fullscreen(true);
+	te.set_resx(1366);
+	te.set_resy(768);
+	te.set_fullscreen(false);
 
 	te.start_renderer(TE_RENDERER_SOFTWARE_RASTERIZER);
 
 	//reading test font
 	t3v::font font("../assets/fonts/OpenSans-Regular.ttf");
 	std::string text="T3Vtech - software renderer";
-
-	t3v::sprite hase_sprite("../assets/Haeschen1.png");
-
-
-	t3v::object3d box;
-	box.make_cube(3);
-	box.position({0,0,4});
-	t3v::texture *box_tex=t3v::load_texture("../assets/wooden_crate_small.jpg");
-	box.use_texture(box_tex);
-
-
-	//loading assets
-	t3v::object2d hase;
-	hase.use_sprite(hase_sprite);
-	hase.position(900,400);
-	hase.scale(4);
-
-	t3v::object2d hase2=hase;
-	hase2.position(350,125);
-	hase2.scale(40);
 
 	te.activate_fps_counter(true, &font);
 
