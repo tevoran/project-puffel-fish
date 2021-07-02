619c9105ae09c376167ab81de1e78868e2a04cec
Bakasilisk <laurent.kuffert@gmail.com>
2021-07-02 23:58:00 +0200

    add hurdles

--- src/game/world.cpp
+++ src/game/world.cpp
@@ -8,12 +8,19 @@ ppf::world::world()
 	m_bottom_hurdle.position(500, 600);
 	m_bottom_hurdle.scale(1.6);
 
+	m_bottom_hurdle1.use_sprite(*m_bottom_hurdle_sprite);
+	m_bottom_hurdle1.position(700,450);
+	m_bottom_hurdle1.scale(2.5);
 
 	//top
 	m_top_hurdle_sprite=new t3v::sprite("../assets/bottom_top.bmp");
 	m_top_hurdle.use_sprite(*m_top_hurdle_sprite);
 	m_top_hurdle.position(600, 0);
 	m_top_hurdle.scale(1.6);
+
+	m_top_hurdle1.use_sprite(*m_top_hurdle_sprite);
+	m_top_hurdle1.position(1000,0);
+	m_top_hurdle1.scale(3);
 }
 
 ppf::world::~world()
@@ -26,5 +33,6 @@ void ppf::world::render()
 {
 	m_bottom_hurdle.render();
 	m_top_hurdle.render();
-
+	m_bottom_hurdle1.render();
+	m_top_hurdle1.render();
 }
