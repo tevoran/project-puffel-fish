1f07f95382f8fb6ef78fc8565cef239a6525e59d
tevoran <j.pfeiffer93@gmx.de>
2021-07-03 10:58:40 +0200

    world spawns new stuff after a while

--- src/game/world.cpp
+++ src/game/world.cpp
@@ -47,6 +47,36 @@ void ppf::world::render(float t_delta)
 	static float time_elapsed=0;
 	time_elapsed+=t_delta;
 
+	//if first time period is over spawn new top hurdle
+	static bool new_top_spawned=false;
+	if(time_elapsed>30 && new_top_spawned==false)
+	{
+		new_top_spawned=true;
+		ppf::world::world_element tmp_top_element;
+		tmp_top_element.object=m_top_hurdle;
+		tmp_top_element.xPos=te.get_resx()+rand()%(te.get_resx()/2);
+		tmp_top_element.yPos=-WORLD_ELEMENT_SIZE_Y+rand()%WORLD_ELEMENT_SIZE_Y;
+		tmp_top_element.xVel=-GAME_SPEED_HORIZONTAL;
+		tmp_top_element.yVel=0;
+		tmp_top_element.object.position(tmp_top_element.xPos, tmp_top_element.yPos);
+		m_top.push_back(tmp_top_element);
+	}
+	//if the second time period is over then spawn a new bottom hurdle
+	if(time_elapsed>60)
+	{
+		time_elapsed=0;
+		new_top_spawned=false;
+
+		ppf::world::world_element tmp_bottom_element;
+		tmp_bottom_element.object=m_bottom_hurdle;
+		tmp_bottom_element.xPos=te.get_resx()+rand()%(te.get_resx()/2);
+		tmp_bottom_element.yPos=te.get_resy()-rand()%WORLD_ELEMENT_SIZE_Y;
+		tmp_bottom_element.xVel=-GAME_SPEED_HORIZONTAL;
+		tmp_bottom_element.yVel=0;
+		tmp_bottom_element.object.position(tmp_bottom_element.xPos, tmp_bottom_element.yPos);
+		m_bottom.push_back(tmp_bottom_element);
+	}
+
 	//updating and render world elements
 	//top
 	for(int i=0; i<m_top.size(); i++)
@@ -59,7 +89,7 @@ void ppf::world::render(float t_delta)
 		//respawn
 		if(m_top[i].xPos<-WORLD_ELEMENT_SIZE_X)
 		{
-			m_top[i].xPos=te.get_resx()+rand()%(te.get_resx()/2);
+			m_top[i].xPos=te.get_resx()+rand()%(te.get_resx());
 			m_top[i].yPos=-WORLD_ELEMENT_SIZE_Y+rand()%WORLD_ELEMENT_SIZE_Y;
 		}
 	}
@@ -75,7 +105,7 @@ void ppf::world::render(float t_delta)
 		//respawn
 		if(m_bottom[i].xPos<-WORLD_ELEMENT_SIZE_X)
 		{
-			m_bottom[i].xPos=te.get_resx()+rand()%(te.get_resx()/2);
+			m_bottom[i].xPos=te.get_resx()+rand()%(te.get_resx());
 			m_bottom[i].yPos=te.get_resy()-rand()%WORLD_ELEMENT_SIZE_Y;
 		}
 	}
