398aa1621a23299c33fe628c01816fc1c8b79362
tevoran <j.pfeiffer93@gmx.de>
2021-07-04 13:47:30 +0200

    added full controls

--- src/game/menu.cpp
+++ src/game/menu.cpp
@@ -42,12 +42,14 @@ void ppf::menu::run(ppf::background& background, ppf::time& time, t3v::font& fon
                               {0,255,255,255},108,400,50);
 		//controls
 	    te.print_single_frame("Controls", font,
-                              {0,255,255,255},32,250,200);
+                              {0,255,255,255},48,100,200);
 	    te.print_single_frame("SPACE for jump", font,
-                              {0,255,255,255},32,250,300);
+                              {0,255,255,255},32,100,300);
+	    te.print_single_frame("Q for changing size", font,
+                              {0,255,255,255},32,100,400);
 	    te.print_single_frame("ENTER for starting the game", font,
-                              {0,255,255,255},32,250,400);
+                              {0,255,255,255},32,100,500);
 	    te.print_single_frame("ESCAPE for ending the game", font,
-                              {0,255,255,255},32,250,500);
+                              {0,255,255,255},32,100,600);
 	}
 }
