85ab0239384f799b8453314c71322184a7b5ac6c
tevoran <j.pfeiffer93@gmx.de>
2021-07-04 13:39:18 +0200

    added control description in the main menu

--- src/game/menu.cpp
+++ src/game/menu.cpp
@@ -37,7 +37,17 @@ void ppf::menu::run(ppf::background& background, ppf::time& time, t3v::font& fon
 		background.render(time.get_delta());
 
 		//text
+		//game title
 	    te.print_single_frame("Puff Puff", font,
-                              {0,255,255,255},108,400,100);
+                              {0,255,255,255},108,400,50);
+		//controls
+	    te.print_single_frame("Controls", font,
+                              {0,255,255,255},32,250,200);
+	    te.print_single_frame("SPACE for jump", font,
+                              {0,255,255,255},32,250,300);
+	    te.print_single_frame("ENTER for starting the game", font,
+                              {0,255,255,255},32,250,400);
+	    te.print_single_frame("ESCAPE for ending the game", font,
+                              {0,255,255,255},32,250,500);
 	}
 }
